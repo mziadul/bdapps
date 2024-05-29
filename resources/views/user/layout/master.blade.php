22c3f5f57a6e25954e1653716299b416ef873ee1
Md. Sohel Akter <sohelrakib929@gmail.com>
2024-05-29 16:46:21 +0600

    google

--- resources/views/user/layout/master.blade.php
+++ resources/views/user/layout/master.blade.php
@@ -19,6 +19,16 @@
   /></noscript>
   <!-- End Meta Pixel Code -->
 
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-47WL0K5P9V"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-47WL0K5P9V');
+  </script>
+
   <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="description" content="bdapps Android Academy">
